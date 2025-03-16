1a72cbc55ec7abdbf752d3446e0623dba92e1dc4
ruslan.nedzelskyi <ruslan.nedzelskyi@gmail.com>
2025-03-16 15:57:12 +0200

    Added information to tech card section

--- src/components/tech_card.tsx
+++ src/components/tech_card.tsx
@@ -1,7 +1,24 @@
 import React, { forwardRef } from 'react';
+import TechCard1 from '../assets/images/macbook _tech_card_1.svg';
+import TechCard2 from '../assets/images/macbook _tech_card_2.svg';
+import TechCard3 from '../assets/images/macbook _tech_card_3.svg';
+import TechCard4 from '../assets/images/macbook _tech_card_4.svg';
 
 const TechCard = forwardRef<HTMLDivElement>((props, ref) => {
-  return <div ref={ref} className="tech_card_page">Вміст TechCard</div>;
+  return <div ref={ref} className="tech_card_page">
+    <div className='tech_card_content_container'>
+      <div className='tech_card_title'>Технологічні карти</div>
+      <div className='tech_card_text_container'>
+        <div className='tech_card_text'>Автоматичне створення та прорахунок собівартості</div>
+        <div className='tech_card_text'>Підвищення ефективності кожної операції</div>
+        <div className='tech_card_text'>Аналіз та рекомендації</div>
+        <div className='tech_card_text'>Моніторинг та коригування в реальному часі</div>
+      </div>
+    </div>
+    <div className='tech_card_img_container'>
+      <img className="logo" src={TechCard1} alt="Tech Card" />
+    </div>
+  </div>;
 });
 
 export default TechCard;
