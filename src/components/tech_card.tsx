cf52eefa56a98a73fcfcfafcfab5d669d18082f6
ruslan.nedzelskyi <ruslan.nedzelskyi@gmail.com>
2025-03-17 17:04:38 +0200

    Added tech card section

--- src/components/tech_card.tsx
+++ src/components/tech_card.tsx
@@ -1,22 +1,48 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
 import TechCard1 from '../assets/images/macbook _tech_card_1.svg';
 import TechCard2 from '../assets/images/macbook _tech_card_2.svg';
 import TechCard3 from '../assets/images/macbook _tech_card_3.svg';
 import TechCard4 from '../assets/images/macbook _tech_card_4.svg';
 
 const TechCard = forwardRef<HTMLDivElement>((props, ref) => {
+  const items = [1, 2, 3, 4];
+  const [activeSlide, setActiveSlide] = useState(1);
+
   return <div ref={ref} className="tech_card_page">
     <div className='tech_card_content_container'>
-      <div className='tech_card_title'>Технологічні карти</div>
       <div className='tech_card_text_container'>
-        <div className='tech_card_text'>Автоматичне створення та прорахунок собівартості</div>
-        <div className='tech_card_text'>Підвищення ефективності кожної операції</div>
-        <div className='tech_card_text'>Аналіз та рекомендації</div>
-        <div className='tech_card_text'>Моніторинг та коригування в реальному часі</div>
+        <span className='tech_card_title'>Технологічні</span>
+        <br />
+        <span className='tech_card_title'>карти</span>
+        <div className='tech_card_sub_text_container'>
+          <div className='tech_card_text_item'>
+            <span className='tech_card_text'>Автоматичне створення</span>
+            <br />
+            <span className='tech_card_text'>та прорахунок собівартості</span>
+          </div>
+          <div className='tech_card_text_item'>
+            <span className='tech_card_text'>Підвищення ефективності</span>
+            <br />
+            <span className='tech_card_text'>кожної операції</span>
+          </div>
+          <div className='tech_card_text_item'>
+            <span className='tech_card_text'>Аналіз та рекомендації</span>
+          </div>
+          <div className='tech_card_text_item'>
+            <span className='tech_card_text'>Моніторинг та коригування</span>
+            <br />
+            <span className='tech_card_text'>в реальному часі</span>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className='tech_card_img_container'>
-      <img className="logo" src={TechCard1} alt="Tech Card" />
+      <div className='tech_card_img_container'>
+        <img className="logo_card_img" src={TechCard1} alt="Tech Card" />
+        <div className='menu_slides_container'>
+          {items.map((item, index) => (
+            <div key={item} className={`menu-item item-${item}`}></div>
+          ))}
+        </div>
+      </div>
     </div>
   </div>;
 });
