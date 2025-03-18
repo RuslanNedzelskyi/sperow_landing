fdaa1c419eb5ff6103cb47655a84d2b8ce2b94a7
ruslan.nedzelskyi <ruslan.nedzelskyi@gmail.com>
2025-03-19 01:39:35 +0200

    Added command section

--- src/components/advantages.tsx
+++ src/components/advantages.tsx
@@ -1,7 +1,8 @@
 import React, { forwardRef } from 'react';
+import advantagesImage from '../assets/images/advantages.jpg';
 
 const Advantages = forwardRef<HTMLDivElement>((props, ref) => {
-  return <div ref={ref} className="advantages_page">Вміст Advantages</div>;
+  return <div ref={ref} className="advantages_page" style={{ backgroundImage: `url(${advantagesImage})` }}></div>;
 });
 
 export default Advantages;
--- src/components/command.tsx
+++ src/components/command.tsx
@@ -1,7 +1,32 @@
-import React, { forwardRef } from 'react';
+import { forwardRef } from 'react';
+import Olya from '../assets/images/Olya.svg';
+import Evgen from '../assets/images/Evgen.svg';
+import Illya from '../assets/images/Illya.svg';
+import Kyril from '../assets/images/Kyril.svg';
+import Victoria from '../assets/images/Victoria.svg';
 
 const Command = forwardRef<HTMLDivElement>((props, ref) => {
-  return <div ref={ref} className="command_page">Вміст Command</div>;
+  return <div ref={ref} className="command_page">
+    <div className='three_images_container'>
+      <div className='command_image_container'>
+        <img className="command_image" src={Kyril} alt="Kyril" />
+      </div>
+      <div className='command_image_container'>
+        <img className="command_image" src={Illya} alt="Illya" />
+      </div>
+      <div className='command_image_container'>
+        <img className="command_image" src={Victoria} alt="Victoria" />
+      </div>
+    </div>
+    <div className='two_images_container'>
+      <div className='command_image_container'>
+        <img className="command_image" src={Olya} alt="Olya" />
+      </div>
+      <div className='command_image_container'>
+        <img className="command_image" src={Evgen} alt="Evgen" />
+      </div>
+    </div>
+  </div>;
 });
 
 export default Command;
