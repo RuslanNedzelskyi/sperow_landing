287defc36367735cc1ae2cb3c7d2752f088fc12d
ruslan.nedzelskyi <ruslan.nedzelskyi@gmail.com>
2025-03-18 21:57:17 +0200

    Added values section, fixed css for agro ai section

--- src/components/values.tsx
+++ src/components/values.tsx
@@ -1,7 +1,12 @@
 import React, { forwardRef } from 'react';
+import values from '../assets/images/values.svg';
 
 const Values = forwardRef<HTMLDivElement>((props, ref) => {
-  return <div ref={ref} className="values_page">Вміст Values</div>;
+  return <div ref={ref} className="values_page">
+    <div className='values_image_container'>
+      <img className="values_image" src={values} alt="Values" />
+    </div>
+  </div>;
 });
 
 export default Values;
