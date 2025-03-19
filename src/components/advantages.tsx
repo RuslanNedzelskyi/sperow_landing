f8c9e6880d00c099243a2e8e09496ebb5479cafa
ruslan.nedzelskyi <ruslan.nedzelskyi@gmail.com>
2025-03-19 18:20:42 +0200

    Added advantages sections

--- src/components/advantages.tsx
+++ src/components/advantages.tsx
@@ -1,16 +1,17 @@
 import React, { forwardRef } from 'react';
 import advantagesImage from '../assets/images/advantages.jpg';
 import advantagesLeft from '../assets/images/advantages_left.svg';
+import advantagesRight from '../assets/images/advantages_right.svg';
 
 const Advantages = forwardRef<HTMLDivElement>((props, ref) => {
   return <div ref={ref} className="advantages_page" style={{ backgroundImage: `url(${advantagesImage})` }}>
-    <div className='advantages_title'>Наші переваги</div>
-    <div>
+    <div className='advantages_images_container'>
       <div className='advantages_left_image_container'>
-        <img className="advantages_left_image" src={advantagesLeft} alt="Victoria" />
+        <img className="advantages_left_image" src={advantagesLeft} alt="Advantages" />
       </div>
+      <div className='advantages_title'>Наші переваги</div>
       <div className='advantages_right_image_container'>
-        {/* <img className="advantages_right_image" src={Victoria} alt="Victoria" /> */}
+      <img className="advantages_right_image" src={advantagesRight} alt="Advantages" />
       </div>
     </div>
   </div>;
