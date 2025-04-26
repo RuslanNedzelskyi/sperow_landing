21718f735d5e611d8af6f8eb6c03d867882cc64b
ruslan.nedzelskyi <ruslan.nedzelskyi@gmail.com>
2025-04-27 01:49:14 +0300

    Added icons to index

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./i18n";
+import logoLight from './assets/images/logo_light.svg';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <App />
+    <head>
+      <meta charSet="utf-8" />
+      <link rel="icon" href={logoLight} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Sperow</title>
+    </head>
+
+    <App />
   </React.StrictMode>
 );
 
